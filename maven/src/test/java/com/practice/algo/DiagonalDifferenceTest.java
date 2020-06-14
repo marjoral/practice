b5969dd6c50af584997677618bb39b1a22fdd687
Luke Marjoram <marjoral@tcd.ie>
2020-06-14 23:38:20 +0100

    Diagonal Difference - Fix tests

--- maven/src/test/java/com/practice/algo/DiagonalDifferenceTest.java
+++ maven/src/test/java/com/practice/algo/DiagonalDifferenceTest.java
@@ -14,7 +14,6 @@ class DiagonalDifferenceTest {
     @Test
     void test_compute_difference_returns_zero_when_diagonals_match() {
         List<List<Integer>> matrix = new ArrayList<>();
-        matrix.add(Collections.singletonList(3));
         matrix.add(Arrays.asList(1,1,1));
         matrix.add(Arrays.asList(1,1,1));
         matrix.add(Arrays.asList(1,1,1));
@@ -27,7 +26,6 @@ class DiagonalDifferenceTest {
     @Test
     void test_compute_difference_returns_sum_when_diagonals_differ() {
         List<List<Integer>> matrix = new ArrayList<>();
-        matrix.add(Collections.singletonList(3));
         matrix.add(Arrays.asList(11,2,4));
         matrix.add(Arrays.asList(4,5,6));
         matrix.add(Arrays.asList(10,8,-12));
@@ -41,7 +39,6 @@ class DiagonalDifferenceTest {
     void test_compute_difference_returns_zero_with_single_element_matrix() {
         List<List<Integer>> matrix = new ArrayList<>();
         matrix.add(Collections.singletonList(1));
-        matrix.add(Collections.singletonList(1));
 
         int difference = DiagonalDifference.computeDifference(matrix);
 
@@ -51,7 +48,6 @@ class DiagonalDifferenceTest {
     @Test
     void test_compute_difference_returns_difference_with_two_by_two_matrix() {
         List<List<Integer>> matrix = new ArrayList<>();
-        matrix.add(Collections.singletonList(2));
         matrix.add(Arrays.asList(1,1));
         matrix.add(Arrays.asList(1,2));
 
